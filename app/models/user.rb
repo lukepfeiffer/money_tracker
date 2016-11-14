5bf2b07920ff770e65f36c0d209001bfa2d665ab
Hashrocket Workstation <dev@hashrocket.com>
2016-11-14 18:11:48 -0500

    User signs up and in

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,18 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
 
   validates_confirmation_of :password
-  validates_presence_of :password, :email
+  validates_presence_of :password, on: :create
+  validates_presence_of :email
   validates_uniqueness_of :email, on: :create
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :users
+  resources :sessions
+
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+
 end
