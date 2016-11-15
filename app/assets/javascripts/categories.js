327c4faf06903f4d1dc052e409f615f01180fb5e
Hashrocket Workstation <dev@hashrocket.com>
2016-11-15 13:28:51 -0500

    User hides form by cancelling

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
   $('#category').on('click', '#add-category', function(event) {
     event.preventDefault();
-    debugger
-    $('.hidden-form').toggle();
+    $('#add-category').hide();
+    $('#cancel').show();
+    $('.hidden-form').show();
   });
+
+  $('#category').on('click', '#cancel', function(event) {
+    event.preventDefault();
+    $('#cancel').hide();
+    $('#add-category').show();
+    $('.hidden-form').hide();
+  });
+
 });
