eb040c6a2f84f6a251a1cccc3ec78b28b0d40855
Hashrocket Workstation <dev@hashrocket.com>
2016-11-27 23:08:20 -0500

    Add funds WIP

--- app/controllers/money_records_controller.rb
+++ app/controllers/money_records_controller.rb
@@ -1,15 +1,19 @@
 class MoneyRecordsController < ApplicationController
 
   def create
-    require 'pry'; binding.pry;
     money_record = MoneyRecord.new(money_record_params)
     money_record.category_id = params[:category_id]
-    category = Category.find(params[:id])
+    category = Category.find(params[:category_id])
+    adjust_category_amount(category, money_record)
     if money_record.save
-      render partial: 'category_container', locals: {categories: category}
+      render partial: 'categories/category_container', locals: {categories: category}
     else
       head :no_content
     end
+  end
+
+  def adjust_category_amount(category, money_record)
+    category.update(amount: category.amount + money_record.amount)
   end
 
   private
