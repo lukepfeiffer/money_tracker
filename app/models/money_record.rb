ff5debcf8e1f139ea0e4bb0d3fa349eb7b64e6a1
Hashrocket Workstation <dev@hashrocket.com>
2016-11-29 13:41:44 -0500

    Filter between active and all records

--- app/models/money_record.rb
+++ app/models/money_record.rb
@@ -1,3 +1,13 @@
 class MoneyRecord < ActiveRecord::Base
   belongs_to :category
+
+  def self.sum(records)
+    sum = 0
+    records.first.each do |record|
+      if record.amount.present?
+        sum = sum + record.amount
+      end
+    end
+    return sum
+  end
 end
