b063bfe3e913599db2ebd01cf7880e8f599bd4d0
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-07-18 11:23:43 -0400

    Refactor money_record model

--- app/models/money_record.rb
+++ app/models/money_record.rb
@@ -2,29 +2,26 @@ class MoneyRecord < ActiveRecord::Base
   belongs_to :category
 
   def self.sum(records)
-    records.sum { |r| r.amount.nil? ? 0 : r.amount.to_d }
+    records.inject(0) { |sum, r| r.amount.nil? ? sum + 0 : sum + r.amount }
   end
 
   def self.filter(user, start_date, end_date, category)
     records = []
+
     if category.present?
-      if category.money_records.present?
-        records = category.money_records
-      end
+      records = category.money_records.where(adjusted_date: start_date...end_date)
     else
       categories = user.categories.active
+
       categories.active.each do |category|
         category.money_records.where(adjusted_date: start_date...end_date).each do |money_record|
           records << money_record
         end
-
       end
+
     end
-    if records == []
-      user.money_records.last
-    else
-      return records
-    end
+
+    records
   end
 
 end
