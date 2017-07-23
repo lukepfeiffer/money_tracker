7c775c462406996bc8efa1ad9ee6461df2cd3b50
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-07-23 09:06:40 -0400

    Quick refactor

--- app/models/money_record.rb
+++ app/models/money_record.rb
@@ -6,22 +6,18 @@ class MoneyRecord < ActiveRecord::Base
   end
 
   def self.filter(user, start_date, end_date, category)
-    records = []
-
     if category.present?
       records = category.money_records.where(adjusted_date: start_date...end_date)
     else
       categories = user.categories.active
 
-      categories.active.each do |category|
+      categories.active.each_with_object([]) do |category, records|
         category.money_records.where(adjusted_date: start_date...end_date).each do |money_record|
           records << money_record
         end
       end
 
     end
-
-    records
   end
 
 end
